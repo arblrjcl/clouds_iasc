9f243eff166d46f72b2844d311da86d68c1d91ce
arblrjcl <arblr@instruction.com>
2024-12-22 14:39:06 +0530

    Initial files to begin

---
+++ using_terraform/tf_civo_environments/tf_staging_civo_root_module/common/twingate_provider.tf
@@ -0,0 +1,7 @@
+
+provider "twingate" {
+  api_token      = var.twingate_api_token
+  network        = "arblr"
+  http_max_retry = 10 # Specifies a retry limit for the http requests made. The default value is 10
+  http_timeout   = 35 # Specifies a time limit in seconds for the http requests made. The default value is 35 seconds.
+}
