6b7f48ead7358695be7e409d7c5451b0834d7bc7
arblrjcl <arblr@instruction.com>
2025-01-04 22:55:33 +0530

    Initial few AWS resources related files

---
+++ using_terraform/tf_aws_environments/tf_staging_aws_root_module/4_eks/eks_main.tf
@@ -0,0 +1,13 @@
+
+module "eks_instance" {
+  source                              = "../../../tf_modules/aws/eks/"
+  project_name                        = local.common_tags.PROJECT_NAME
+  eks_iam_assume_role_tags            = merge(local.common_tags, { "TF_LAYER_NAME" : var.tf_layer_name })
+  eks_kubernetes_version              = "1.30"
+  eks_cluster_tags                    = merge(local.common_tags, { "TF_LAYER_NAME" : var.tf_layer_name })
+  eks_workers_node_group_tags         = merge(local.common_tags, { "TF_LAYER_NAME" : var.tf_layer_name })
+  eks_developer_group_access_tags     = merge(local.common_tags, { "TF_LAYER_NAME" : var.tf_layer_name })
+  eks_cluster_endpoint_private_access = var.eks_cluster_endpoint_private_access
+  # https://docs.aws.amazon.com/eks/latest/userguide/managing-vpc-cni.html#vpc-cni-latest-available-version
+  eks_addon_vpc_cni_version = "v1.19.0-eksbuild.1"
+}
