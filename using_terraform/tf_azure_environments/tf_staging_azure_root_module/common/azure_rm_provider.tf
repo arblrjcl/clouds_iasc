8cf732bddfb44390529a8b7bfe082f41fffd7dbb
arblrjcl <arblr@instruction.com>
2025-02-07 09:10:09 +0530

    Initial Azure project setup files

---
+++ using_terraform/tf_azure_environments/tf_staging_azure_root_module/common/azure_rm_provider.tf
@@ -0,0 +1,7 @@
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "00000000-0000-0000-0000-000000000000"  //  We need to correct subscription_id
+}
+
