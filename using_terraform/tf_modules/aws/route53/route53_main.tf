a4602d6270976a60be6dcad375e95041c6252177
arblrjcl <arblr@instruction.com>
2025-04-09 19:14:57 +0530

    s3 gateway endpoint and DNSSEC updates

--- using_terraform/tf_modules/aws/route53/route53_main.tf
+++ using_terraform/tf_modules/aws/route53/route53_main.tf
@@ -57,3 +57,53 @@ resource "aws_route53_record" "private_hosted_zone_delegation" {
   depends_on = [aws_route53_zone.environment_specific_private_hosted_zone]
 }
 
+data "aws_caller_identity" "current_user_caller_identity" {}
+
+# AWS Asymmetric KMS Key  https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/kms_key
+resource "aws_kms_key" "public_hosted_zone_dnssec_kms_cmk" {
+  description              = "ECC_NIST_P256 asymmetric KMS key for signing and verification"
+  deletion_window_in_days  = 7
+  customer_master_key_spec = "ECC_NIST_P256"
+  key_usage                = "SIGN_VERIFY"
+  enable_key_rotation      = false
+  tags                     = var.public_hosted_zone_tags
+  policy = jsonencode({
+    Statement = [
+      {
+        Action = [
+          "kms:Sign",
+          "kms:Verify",
+          "kms:DescribeKey",
+          "kms:GetPublicKey",
+        ],
+        Effect = "Allow"
+        Principal = {
+          Service = "dnssec-route53.amazonaws.com"
+        }
+        Resource = "*"
+        Sid      = "Allow Route 53 DNSSEC Service",
+      },
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current_user_caller_identity.account_id}:root"
+        }
+        Resource = "*"
+        Sid      = "Enable IAM User Permissions"
+      },
+    ]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_route53_key_signing_key" "public_hosted_zone_dnssec_ksk" {
+  hosted_zone_id             = aws_route53_zone.public_hosted_zone.zone_id
+  key_management_service_arn = aws_kms_key.public_hosted_zone_dnssec_kms_cmk.arn
+  name                       = "${var.public_hosted_zone_name}-dnssec-ksk"
+}
+
+resource "aws_route53_hosted_zone_dnssec" "enable" {
+  hosted_zone_id = aws_route53_key_signing_key.public_hosted_zone_dnssec_ksk.hosted_zone_id
+  depends_on     = [aws_route53_key_signing_key.public_hosted_zone_dnssec_ksk]
+}
